901fe720d75889167053d9d38fadbfe1b4356085
VicLim <awslimw0144@gmail.com>
2020-03-15 14:21:25 +0800

    Testing my new commit with Gradle v2.

--- src/test/java/com/taiger/testsuite/testsuite001/SimpleRunner.java
+++ src/test/java/com/taiger/testsuite/testsuite001/SimpleRunner.java
@@ -7,6 +7,6 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         plugin = {"pretty"},
-        features = "src/test/resources/feature"
+        features = "src/test/resources/featurea"
 )
 public class SimpleRunner {}
